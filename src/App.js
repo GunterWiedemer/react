9b5bcf1d12cc9c4d0d06f65bf62eba2ad42cd744
GunterWiedemer <109611733+GunterWiedemer@users.noreply.github.com>
2022-07-19 17:27:17 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Comp1 from "./components/Comp1";
+
+
+function App() {
+
+  const text = 'Hello World!';
+
+  return (
+    <div>
+      <Comp1 text={text}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Comp1.js
@@ -0,0 +1,24 @@
+import React, { useState} from 'react';
+
+const Comp1 = (props) => {
+
+const text = props.text;
+
+const clickHandler = () => {
+    console.log('Hello!!!')
+}
+const [count, setCount] = useState(0)
+
+return (
+    <div>
+        <h2>Event Handling</h2>
+        <button onClick={clickHandler}>Push</button>
+        <h2>useState-Hook example</h2>
+        <p>Du hast mich {count} mal geklickt</p>
+        <button onClick={() => setCount(count + 1)}>Click</button>
+    </div>
+);
+
+}
+
+export default Comp1
